a89bc2fcf890da90cda388d868f8fb5a6a04739a
blackkitten1 <blackkitten010@gmail.com>
2020-09-14 18:44:42 +0700

    .

--- week6-1/week6-1.cpp
+++ week6-1/week6-1.cpp
@@ -10,8 +10,45 @@ int most(int x, int y, int z) {
 }
 
 int main() {
-	int a,b,c;
+	int a,b,c,s=0;
 	scanf_s("%d %d %d", &a, &b, &c);
-	printf("%d", most(a, b, c));
+	while (a != 1 || b != 1 || c != 1)
+	{
+		if (most(a, b, c) % 2 != 0)
+		{
+			if (most(a, b, c) == a)
+			{
+				a -= 1;
+				a =a/ 2;
+			}
+			else if (most(a, b, c) == b)
+			{
+				b -= 1;
+				b = b / 2;
+			}
+			else if (most(a, b, c) == c)
+			{
+				c -= 1;
+				c = c / 2;
+			}
+		}
+		else if(most(a,b,c)%2==0)
+		{
+			if (most(a, b, c) == a)
+			{
+				a = a / 2;
+			}
+			else if (most(a, b, c) == b)
+			{
+				b = b / 2;
+			}
+			else if (most(a, b, c) == c)
+			{
+				c = c / 2;
+			}
+		}
+		s += 1;
+	}
+	printf("%d", s);
 	return 0;
 }
